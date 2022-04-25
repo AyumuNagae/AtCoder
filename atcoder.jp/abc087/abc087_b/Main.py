608b961455157cb39299bbbefd75488f7738b3c5
tomonio <ayumu100811@gmail.com>
2022-04-25 05:00:50 +0000

    [AC] abc087 abc087_b

---
+++ atcoder.jp/abc087/abc087_b/Main.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
+
+count = 0
+
+for i in range(a+1):
+  sum = 0
+  for j in range(b+1):
+    for k in range(c+1):
+      sum = i*500 + j*100 + k*50
+      if sum == x:
+        count += 1
+print(count)
