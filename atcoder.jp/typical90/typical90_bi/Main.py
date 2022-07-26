4eb0838b9c6df2e53a2957ba76f18659cb31a484
tomonio <ayumu100811@gmail.com>
2022-07-26 02:13:03 +0000

    [AC] typical90 typical90_bi

---
+++ atcoder.jp/typical90/typical90_bi/Main.py
@@ -0,0 +1,8 @@
+Q = int(input())
+from collections import deque
+q = deque()
+for i in range(Q):
+  t, x = map(int,input().split())
+  if   t == 1:q.appendleft(x)
+  elif t == 2:q.append(x)
+  elif t == 3:print(q[x - 1])
