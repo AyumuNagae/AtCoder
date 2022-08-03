e5befdcb99a1c4baa3f2babfcc0565566b75486b
tomonio <ayumu100811@gmail.com>
2022-08-03 02:33:09 +0000

    [AC] typical90 typical90_ax

---
+++ atcoder.jp/typical90/typical90_ax/Main.py
@@ -0,0 +1,12 @@
+n, l = map(int,input().split())
+dp = [0] * (n + 1)
+dp[0] = 1
+mod = 10 ** 9 + 7
+for i in range(1, n + 1):
+    if i < l:
+        dp[i] = dp[i - 1]
+    else:
+        dp[i] = dp[i - 1] + dp[i - l]
+    dp[i] %= mod
+
+print(dp[n])
